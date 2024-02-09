8b579859ecfaf6c9e98a1472fa130dc3d17ca166
Marilu1707 <154383008+Marilu1707@users.noreply.github.com>
2024-02-08 21:02:30 -0300

    Delete project directory

--- project/apps/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from accounts.models import UserExtension
-
-admin.site.register(UserExtension)
-
--- project/apps/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    context = {"app_name": "Spiderweb"}
-    return render(request, "core/index.html",context)
